8f3da812b6ffd7145220ab2e9455ece32b2c2f23
pranav <rajpranav424@gmail.com>
2024-06-13 22:26:30 +0530

    task1

--- main.py
+++ main.py
@@ -17,21 +17,46 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
             ID = input("ID:")
             city = input("City:")
             branchcodes = input("Branch(es):")
-            # How will you conver this to a list, given that
+            branchcodes = [int(code.strip()) for code in branchcodes.split(',')]
+            # How will you convert this to a list, given that
             # the user will always enter a comma separated list of branch codes?
             # eg>   2,5
 
             salary = input("Salary: ")
+            age=input("age:")
+            position=input("positon is:")
             # Create a new Engineer with given details.
-            engineer = None # Change this
+            engineer = Engineer(name, age,ID,city,branchcodes,position,salary) # Change this
 
             engineer_roster.append(engineer) # Add him to the list! See people.py for definiton
             
         
+
+
         elif last_input == 2:
+         
+
+
+            name = input("Name:")
+            ID = input("ID:")
+            city = input("City:")
+            branchcodes = input("Branch(es):")
+            branchcodes = [int(code.strip()) for code in branchcodes.split(',')]
+
+            salary = input("Salary: ")
+            age=input("age:")
+            position=input("positon is:")    
+
+
+            salesman = Salesman(name, age,ID,city,branchcodes,position,salary)
+            sales_roster.append(salesman)
+
+
+
+
             # Gather input to create a Salesperson
             # Then add them to the roster
-            pass
+            
 
         elif last_input == 3:
             ID = int(input("ID: "))
@@ -51,10 +76,11 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
                 ## Write code here to list the branch names to
                 ## which the employee reports as a comma separated list
                 ## eg> Branches: Goregaon,Fort
-                branch_names = []
+                branch_names = [branchmap[code]['name'] for code in found_employee.branches]
+
                 ## ???? what comes here??
                 # print(f"Branches: " + ???? )
-                
+                print(f"Branches: {', '.join(branch_names)}")
                 print(f"Salary: {found_employee.salary}")
 
         elif last_input == 4:
@@ -62,24 +88,42 @@ if __name__ == "__main__":  # Equivalent to int main() {} in C++.
             # Change branch to new branch or add a new branch depending on class
             # Inheritance should automatically do this. 
             # There should be no IF-ELSE or ternary operators in this zone
+            ID = int(input("Enter Employee ID to promote: "))
+             
             pass
             #### NO IF ELSE ZONE ENDS #################################################
 
         elif last_input == 5:
             ID = int(input("Enter Employee ID to promote: "))
+            position=input("promote to:")
+            for employee in engineer_roster + sales_roster:
+                if employee.ID == ID:
+                    employee.promote(position)
             # promote employee to next position
 
         elif last_input == 6:
-            ID = int(input("Enter Employee ID to give increment: "))
+            ID = int(input("Enter Employee I  D to give increment: "))
+            increment_amt=int(input("increment should be"))
+            for employee in engineer_roster + sales_roster:
+                if employee.ID == ID:
+                    employee.increment(increment_amt)
             # Increment salary of employee.
         
         elif last_input == 7:
             ID = int(input("Enter Employee ID to find superior: "))
+            for employee in sales_roster:
+                if employee.ID == ID:
+                    employee.find_superior()
+
             # Print superior of the sales employee.
         
         elif last_input == 8:
-            ID_E = int(input("Enter Employee ID to add superior: "))
-            ID_S = int(input("Enter Employee ID of superior: "))
+            ID = int(input("Enter Employee ID to add superior: "))
+           
+            for employee in sales_roster:
+                if employee.ID == ID:
+                     employee.add_superior()
+
             # Add superior of a sales employee
 
         else:
--- people.py
+++ people.py
@@ -1,3 +1,4 @@
+from typing import Optional
 """
 We'll try to understand classes in python. 
 Check the resources on google classroom to ensure you have gone through everything expected.
@@ -33,21 +34,37 @@ class Employee:
         self.branches = branchcodes
         if salary is not None: self.salary = salary
         else: self.salary = 10_000 
+        
     
     def change_city(self, new_city:str) -> bool:
+        if self.city != new_city:
+            self.city = new_city
+            return True
+        else:
+            return False
         # Change the city 
         # Return true if city change, successful, return false if city same as old city
-        pass
+       
 
     def migrate_branch(self, new_code:int) -> bool:
+        if len(self.branches)!=1:
+            return False
+        else :
+            current_branch_code = self.branches[0]
+            if branchmap[current_branch_code]['city'] == branchmap[new_code]['city']:
+                self.branches[0] = new_code
+                return True
+            else:
+                return False
         # Should work only on those employees who have a single 
         # branch to report to. Fail for others.
         # Change old branch to new if it is in the same city, else return false.
-        pass
+        
 
     def increment(self, increment_amt: int) -> None:
         # Increment salary by amount specified.
-        pass
+        self.salary+=increment_amt
+        
 
 
 
@@ -58,19 +75,41 @@ class Engineer(Employee):
 
     def __init__(self, name, age, ID, city,\
                  branchcodes, position= "Junior", salary = None):
-        # Call the parent's constructor
+        # Call the parent's constructor  
         super().__init__(name, age, ID, city, branchcodes, salary)
-        
+        positions_allowed=["Junior","Senior", "Team Lead", "Director" ]
+        t=0
+        for pos in positions_allowed:
+            if(position==pos): 
+             self.position=position
+             t=1
+             break
+        if(t==0):
+         raise ValueError(f"Invalid position: {position}")
+        else: 
+         self.t=t
         # Check if position is one of  "Junior", "Senior", "Team Lead", or "Director" 
         # Only then set the position. 
-
+         engineer_roster.append(self)
+        
     
     def increment(self, amt:int) -> None:
+        self.salary+=(amt*1.1)
         # While other functions are the same for and engineer,
         # and increment to an engineer's salary should add a 10% bonus on to "amt"
         pass
         
     def promote(self, position:str) -> bool:
+        positions_allowed=["Junior","Senior", "Team Lead", "Director" ]
+        if position not in positions_allowed:
+            return False
+        current_index = positions_allowed.index(self.position)
+        new_index = positions_allowed.index(position)
+        if new_index <= current_index:
+            return False
+        self.increment(self.salary*0.3)
+        self.position = position
+        return True
         # Return false for a demotion or an invalid promotion
         # Promotion can only be to a higher position and
         # it should call the increment function with 30% of the present salary
@@ -89,31 +128,86 @@ class Salesman(Employee):
     Add an argument in init which tracks who is the superior
     that the employee reports to. This argument should be the ID of the superior
     It should be None for a "Head" and so, the argument should be optional in init.
-    """
+    """ 
     
     # An extra member variable!
     superior : int # EMPLOYEE ID of the superior this guy reports to
-
-    def __init__(self, ): # Complete all this! Add arguments
-        pass
+    superior_id:  Optional[int]
+    def __init__(self, name, age, ID, city,\
+                 branchcodes, position= "Rep", salary = None):
+        # Call the parent's constructor  
+        super().__init__(name, age, ID, city, branchcodes, salary)
+        positions_allowed=["Rep","Manager", "Head"]
+        t=0
+        f=0
+        for pos in positions_allowed:
+            t+=1
+            if(position==pos): 
+             self.position=position
+             f=1
+             break
+        if(t==0):
+         raise ValueError(f"Invalid position: {position}")
+        else: 
+         self.t=t
+         sales_roster.append(self)
+        
+            
     
     # def promote 
-
+    def promote(self, position:str) -> bool:
+        positions_allowed=["Rep","Manager", "Head"]
+        if position not in positions_allowed:
+            return False
+        current_index = positions_allowed.index(self.position)
+        new_index = positions_allowed.index(position)
+        if new_index <= current_index:
+            return False
+        self.increment(self.salary*0.3)
+        self.position = position
+        return True
     # def increment 
+    def increment(self, amt:int) -> None:
+         self.salary+=(amt*1.05)
+        
+
+
+
+
 
     def find_superior(self) -> tuple[int, str]:
+        if(self.t==3):
+            return(None,None)
+        for salesman in sales_roster:
+            if(salesman.t>self.t):
+                return(salesman.ID,salesman.name)
+            
         # Return the employee ID and name of the superior
         # Report a tuple of None, None if no superior.
-        pass
+        
+
+
+
 
     def add_superior(self) -> bool:
+        if(self.t==3):
+            return False
+        for salesman in sales_roster:
+            if(salesman.t>self.t):
+                self.superior_id = salesman.ID
         # Add superior of immediately higher rank.
         # If superior doesn't exist return false,
-        pass
+
+
+
 
 
     def migrate_branch(self, new_code: int) -> bool:
         # This should simply add a branch to the list; even different cities are fine
+        if new_code not in self.branches:
+            self.branches.append(new_code)
+            return True
+        return False
         pass
 
     
